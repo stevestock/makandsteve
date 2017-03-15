11f7d2e7fea34befa6dd3e3d21f7767ea55290c0
Steven Stockhamer <stevestock@gmail.com>
2017-03-14 22:15:34 -0400

    forgot to git add

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: './resources/assets/js/index.js'
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: require.resolve("outdated-browser"),
+        use: 'exports-loader?outdatedBrowser'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        use: "file-loader?name=[name].[ext]&publicPath=../fonts/&outputPath=fonts/"
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: "file-loader?name=[name].[ext]&publicPath=../img/&outputPath=img/"
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname), "node_modules"]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: "jquery"
+    }),
+    new ExtractTextPlugin('css/style.css')
+  ]
+};
